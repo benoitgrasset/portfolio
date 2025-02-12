3f509fd2fe13cd7e6cb4e90b1d8b2fd53bf10fba
Benoit GRASSET <b.grasset@cliqdigital.com>
2025-02-12 19:26:56 +0100

    third commmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,47 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Home() {
+  const projects = [
+    {
+      title: "strava-dashboard",
+      description: "Description 1",
+      image: "/images/1.jpg",
+      link: "https://strava-dashboard.netlify.app/",
+    },
+    {
+      title: "strava-dashboard-compare",
+      description: "Description 2",
+      image: "/images/2.jpg",
+      link: "https://strava-dashboard-compare.netlify.app/",
+    },
+    {
+      title: "ZWO editor",
+      description: "Description 3",
+      image: "/images/3.jpg",
+      link: "https://silver-unicorn-691fa9.netlify.app/",
+    },
+    {
+      title: "life-game",
+      description: "Description 4",
+      image: "/images/4.jpg",
+      link: "https://benoitgrasset.github.io/life-game/",
+    },
+    {
+      title: "Project 5",
+      description: "Description 5",
+      image: "/images/5.jpg",
+      link: "https://example.com/project5",
+    },
+  ];
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -7,6 +50,46 @@ export default function Home() {
           <span className="text-[var(--geist-foreground)]">Benoit</span>, a
           software engineer.
         </h1>
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full max-w-xxl"
+        >
+          <CarouselContent>
+            {projects.map((project, index) => (
+              <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/4">
+                <div className="p-1">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>{project.title}</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-col aspect-square items-center justify-center p-6">
+                      <img
+                        src={project.image}
+                        alt={project.title}
+                        className="w-full h-48 object-cover rounded-lg"
+                      />
+                      <p className="mt-4 text-sm text-gray-500">
+                        {project.description}
+                      </p>
+                      <p>
+                        <a
+                          href={project.link}
+                          className="text-blue-500 hover:underline"
+                        >
+                          {project.link}
+                        </a>
+                      </p>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
